475b2f1a2b3139a24843a47cb4f8fb6352e7ce5c
Leon (Weidong Li) <leonlidawn@gmail.com>
2020-03-02 18:14:15 +1100

    added content for locales

---
+++ src/locales/en-US.js
@@ -0,0 +1,27 @@
+export default {
+	'navigation.home': 'home',
+	'navigation.resume': 'resume',
+	'navigation.works': 'works',
+	'navigation.contacts': 'contacts',
+	'home.banner.array':
+		[
+			'I am a web developer',
+			'I code cool websites',
+			'I enjoy diving into problem and get it solved',
+			'Patience and attention to details makes perfection'
+		]
+	,
+	'home.introduction': 'I love Javascript, React and Node.js',
+	'introduction':
+		`A passionate full-stack web developer who loves delivering solutions with code. I enjoy the excitement of learning new things and always like to share knowledge with others. I am an excellent team player in Agile/Scrum teams. I believe efficient and responsible communications are the fundamentals of good collaboration.`,
+	'skills.array':
+		[
+			'Front-end: HTML5, CSS3, Sass, Less, JavaScript (ES6+), React, Redux, Redux-saga, Redux-thunk, Pug',
+			'Back-end: Node.js, Express.js, KeystoneJS',
+			'Testing: Jest',
+			'Databases: MongoDB, MySQL',
+			'DevOps: Docker',
+			'Tooling: Git, GitHub, NPM, BitBucket, JIRA',
+			'Methodologies: Agile / Scrum / Kanban',
+		],
+};
---
+++ src/locales/zh-CN.js
@@ -0,0 +1,27 @@
+export default {
+	'navigation.home': '首页',
+	'navigation.resume': '简历',
+	'navigation.works': '作品',
+	'navigation.contacts': '联系方式',
+	'home.banner.array':
+		[
+			'在开发的路上',
+			'做最酷的网站',
+			'探不同的迷，寻根源的解',
+			'深信细致与耐心成就完美'
+		]
+	,
+	'home.introduction': '热衷于使用Javascript, React 和 Node.js',
+	'introduction':
+		`充满热诚的网页开发者，善用代码解决问题. 对于知识的态度是乐于探究更乐于分享。团队型码农，熟悉敏捷开发. 深知有效负责的沟通是良好协作的第一基石。`,
+	'skills.array':
+		[
+			'前端: HTML5, CSS3, Sass, Less, JavaScript (ES6+), React, Redux, Redux-saga, Redux-thunk, Pug',
+			'后端: Node.js, Express.js, KeystoneJS',
+			'测试: Jest',
+			'数据库: MongoDB, MySQL',
+			'运维: Docker',
+			'工具: Git, GitHub, NPM, BitBucket, JIRA',
+			'协作模式: Agile / Scrum / Kanban',
+		],
+};
